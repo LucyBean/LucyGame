11c2d4d6d69eccd489dcf2921f70ebcd60863d69
Lucy <howobscene@msn.com>
2016-07-30 13:10:47 +0100

    Walls are now a single object
    They are drawn by repeatedly tiling the standard Wall sprite. This should
    be much more efficient than before as there are fewer rectangles against
    which a moving object must collision check.

--- src/objectLibrary/Wall.java
+++ src/objectLibrary/Wall.java
@@ -1,19 +1,54 @@
 package objectLibrary;
 
 import helpers.Point;
+import helpers.Rectangle;
 import objectLibs.SpriteLibrary;
 import objects.Collider;
+import objects.Sprite;
 import objects.Static;
+import worlds.Camera;
+import worlds.GlobalOptions;
 import worlds.WorldLayer;
 
 public class Wall extends Static {
+	final int width;
+	final int height;
+
 	public Wall(Point origin, int width, int height) {
 		super(origin, WorldLayer.WORLD, SpriteLibrary.WALL,
 				new Collider(Point.ZERO, width, height), null);
+		this.width = width;
+		this.height = height;
 	}
 
 	@Override
 	protected void resetStaticState() {
 
+	}
+
+	/**
+	 * Draws the Wall object.
+	 */
+	@Override
+	public void draw(Camera camera) {
+		// This must be overriden as drawing the wall involves drawing
+		// multiple Wall sprites
+		Sprite sprite = getSprite();
+		Rectangle imageRect = sprite.getBoundingRectangle();
+		Rectangle offset = objectToScreenCoOrds(
+				imageRect.scaleAboutOrigin(1.0f / GlobalOptions.GRID_SIZE),
+				camera);
+		Point imageCoOrds = objectToScreenCoOrds(sprite.getOrigin(), camera);
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				sprite.getImage().draw(
+						imageCoOrds.getX() + offset.getWidth() * x,
+						imageCoOrds.getY() + offset.getHeight() * y,
+						getDrawScale(camera));
+			}
+		}
+
+		drawCollider(camera);
+		drawInteractBox(camera);
 	}
 }
